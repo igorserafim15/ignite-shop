bf0cd437a8c46f550dad0ee223a6117add284646
Igor Serafim <igor.serafim@nolifretes.com.br>
2023-10-09 13:56:55 -0300

    .

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,38 +1,75 @@
-import cam1 from '../assets/camisa-1.png'
-import cam2 from '../assets/camisa-2.png'
-import cam3 from '../assets/camisa-3.png'
-
 import Image from 'next/image'
+import { useKeenSlider } from 'keen-slider/react'
+import { stripe } from '../lib/stripe'
+import { GetStaticProps } from 'next'
+import Stripe from 'stripe'
 
-export default function Home() {
+interface HomeProps {
+  products: {
+    id: string
+    name: string
+    imageUrl: string
+    price: number
+  }[]
+}
+
+export default function Home({ products }: HomeProps) {
+  const [sliderRef] = useKeenSlider({
+    slides: { perView: 3, spacing: 48 },
+  })
+
   return (
-    <div className="flex gap-10 min-h-[656px] w-full ml-auto max-w-app">
-      <div className="bg-product rounded-lg p-2 cursor-pointer relative flex items-center justify-center">
-        <Image src={cam1} alt="" width={520} height={480} />
+    <div
+      className="flex min-h-[656px] w-full ml-auto max-w-app keen-slider"
+      ref={sliderRef}
+    >
+      {products.map((product) => (
+        <a
+          key={product.id}
+          className="keen-slider__slide bg-product rounded-lg  cursor-pointer relative flex items-center justify-center group overflow-hidden"
+        >
+          <Image
+            src={product.imageUrl}
+            alt={product.name}
+            width={520}
+            height={480}
+          />
 
-        <footer className="absolute bottom-1 left-1 right-1 p-8 rounded-md flex items-center justify-between translate-y-[110%] opacity-0 transition-all ease-in-out hover:translate-y-[0%] hover:opacity-100">
-          <strong>Camiseta 1</strong>
-          <span>R$ 79,90</span>
-        </footer>
-      </div>
-
-      <div className="bg-product">
-        <Image src={cam2} alt="" width={520} height={480} />
-
-        <footer className="absolute bottom-1 left-1 right-1 p-8 rounded-md flex items-center justify-between translate-y-[110%] opacity-0 transition-all ease-in-out">
-          <strong>Camiseta 1</strong>
-          <span>R$ 79,90</span>
-        </footer>
-      </div>
-
-      <div className="bg-product">
-        <Image src={cam3} alt="" width={520} height={480} />
-
-        <footer>
-          <strong>Camiseta 1</strong>
-          <span>R$ 79,90</span>
-        </footer>
-      </div>
+          <footer className="absolute bottom-1 left-1 right-1 p-8 rounded-md flex items-center justify-between translate-y-[110%] opacity-0 transition-all ease-in-out group-hover:translate-y-[0%] group-hover:opacity-100 bg-black/50">
+            <strong className="tex-lg">{product.name}</strong>
+            <span className="text-green300 font-bold text-lg">
+              {product.price}
+            </span>
+          </footer>
+        </a>
+      ))}
     </div>
   )
+}
+
+export const getStaticProps: GetStaticProps = async () => {
+  const response = await stripe.products.list({
+    expand: ['data.default_price'],
+  })
+
+  function formatPrice(price: number) {
+    return new Intl.NumberFormat('pt-BR', {
+      style: 'currency',
+      currency: 'BRL',
+    }).format(price)
+  }
+
+  const products = response.data.map((product) => ({
+    id: product.id,
+    name: product.name,
+    imageUrl: product.images[0],
+    price: formatPrice(
+      (product.default_price as Stripe.Price).unit_amount / 100,
+    ),
+  }))
+
+  return {
+    props: { products },
+    revalidate: 60 * 60 * 2, // 2 hourså
+  }
 }
